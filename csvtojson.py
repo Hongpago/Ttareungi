dda63bdc5a47f2cf7fe206eefebf8322b1ba3906
huey.j <huey.j@kakaocorp.com>
2021-06-14 23:49:16 +0900

    시각화 수정 및 json 파일 추가

--- csvtojson.py
+++ csvtojson.py
@@ -12,3 +12,64 @@ with open(input_file_name, "r", encoding="utf-8", newline="") as input_file, \
     for cols in reader:
         doc = {col_name: col for col_name, col in zip(col_names, cols)}
         print(json.dumps(doc, ensure_ascii=False), file=output_file)
+
+
+# c1 = database.rental_office_usage.find()
+# c2 = database.new_sub.find()
+# c3 = database.rental_history.find()
+# c4 = database.rental_office.find()
+#
+# j_dict = {}
+# i = 0
+# for row in c1:
+#     j_dict[i] = {'rental_date(month)': row['rental_date(month)'],
+#                  'rental_office_id': row['rental_office_id'],
+#                  'rental_office_name': row['rental_office_name'],
+#                  'number_of_rentals': row['number_of_rentals']}
+#     i = i + 1
+#
+# with open('rental_office_usage.json', 'w') as file:
+#     json.dump(j_dict, file, indent=4,ensure_ascii = False)
+#
+# print("json 파일 생성")
+#
+# j_dict = {}
+# i = 0
+# for row in c2:
+#     j_dict[i] = {'sub_date': row['sub_date'], 'user_age': row['user_age'], 'sub_num': row['sub_num']}
+#     i = i + 1
+#
+# with open('new_sub.json', 'w') as file:
+#     json.dump(j_dict, file, indent=4, ensure_ascii = False)
+#
+# print("json 파일 생성")
+
+# j_dict = {}
+# i = 0
+# for row in c3:
+#     j_dict[i] = {'bicycle_id': row['bicycle_id'], 'rental_date': row['rental_date'],
+#                  'rental_holder_id': row['rental_holder_id'], 'rental_office_id': row['rental_office_id'],
+#                  'rental_office_name': row['rental_office_name'], 'return_date': row['return_date'],
+#                  'return_holder_id': row['return_holder_id'], 'return_office_id': row['return_office_id'],
+#                  'return_office_name': row['return_office_name'], 'use_distance': row['use_distance'],
+#                  'use_time': row['use_time']}
+#     i = i + 1
+#
+# with open('rental_history.json', 'w') as file:
+#     json.dump(j_dict, file, indent=4, ensure_ascii = False)
+#
+# print("json 파일 생성")
+#
+# j_dict = {}
+# i = 0
+# for row in c4:
+#     j_dict[i] = {'rental_office_id': row['rental_office_id'], 'rental_office_name': row['rental_office_name'],
+#                  'gu': row['gu'], 'detail_address': row['detail_address'], 'latitude': row['latitude'],
+#                  'longitude': row['longitude'], 'installation_date': row['installation_date'],
+#                  'operation_method': row['field9']}
+#     i = i + 1
+#
+# with open('rental_office.json', 'w') as file:
+#     json.dump(j_dict, file, indent=4, ensure_ascii = False)
+#
+# print("json 파일 생성")
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import datetime
 import urllib.parse
+import webbrowser
 
 import folium
 import pandas as pd
@@ -13,7 +14,6 @@ host = "localhost"
 port = "17017"
 username = urllib.parse.quote_plus('admin')
 password = urllib.parse.quote_plus('admin')
-# mongo = MongoClient('mongodb://%s:%s@서버ip:27017')
 mongo = MongoClient(host, int(port))
 database = mongo.Ttareungi
 print(database)
@@ -37,7 +37,7 @@ pipe4 = [{'$group': {'_id': {"year": {"$substr": ["$rental_date", 0, 4]}, "month
 
 pipe5 = [{'$group': {'_id': {"year": {"$substr": ["$return_date", 0, 4]}, "month": {"$substr": ["$return_date", 5, 2]},
                              "return": "$return_office_id"}, "count": {"$sum": 1},
-                     "name": {"$first": "$rental_office_name"}}},
+                     "name": {"$first": "$return_office_name"}}},
          {'$group': {'_id': "$_id.return", "avg": {'$avg': "$count"}}}, {'$sort': {'_id': 1}}]
 # pip3꺼를 설치시기 같은 애들 평균내서 년도별 평균 내기
 install_date_query = {'rental_office_id': 1, 'installation_date': 1}
@@ -47,21 +47,25 @@ location_query = {'rental_office_id': 1, 'rental_office_name': 1, 'latitude': 1,
 # -------------------------------------------------------------------------------------------
 
 use_by_month = database.rental_office_usage.aggregate(pipe1)
+print("쿼리1 완료")
 new_users_by_month = database.new_sub.aggregate(pipe2)
+print("쿼리2 완료")
 use_avg_by_rental_office = database.rental_office_usage.aggregate(pipe3)
+print("쿼리3 완료")
 rental_avg_by_rental_office = database.rental_history.aggregate(pipe4)
+print("쿼리4 완료")
 return_avg_by_rental_office = database.rental_history.aggregate(pipe5)
+print("쿼리5 완료")
 install_date_of_rental_office = database.rental_office.find({}, install_date_query)
+print("쿼리6 완료")
 rental_office_location = database.rental_office.find({}, location_query)
+print("쿼리7 완료")
 
 rental_office_install_date = dict(
     (data['rental_office_id'], data['installation_date']) for data in install_date_of_rental_office)
 rental_office_rental_num = dict()
 
 # -------------------------------------------------------------------------------------------
-
-# print(rental_office)
-
 use_avg_by_install_date = dict()
 count_by_install_date = dict()
 dates = []
@@ -76,14 +80,17 @@ office_id_avg = []
 rental_return_avg_sub = dict()
 office_locations = []
 office_avg_month = dict()
-pipe4_office_id = []
+pipe4_rental_avg = dict()
+pipe4_return_avg = dict()
+pipe4_rental_avg_list = []
+pipe4_return_avg_list = []
 pipe6_office_id = []
 pipe6_office_install_date = []
 pipe6_office_avg_month = []
 pipe6_office_avg = []
-
+group_office_id_high = dict()
+group_office_id_low = dict()
 # -------------------------------------------------------------------------------------------
-
 for data in list(use_avg_by_rental_office):
     rental_office_id = data['office_id']
 
@@ -113,24 +120,46 @@ for data in list(new_users_by_month):
     date.append(datetime.datetime(data['date'] // 100, data['date'] % 100, 1))
 
 for data in list(rental_avg_by_rental_office):
-    # pipe4_office_id.append(data['_id'])
-    # avgs.append(data['avg'])
     rental_return_avg_sub[data['_id']] = data['avg']
+    pipe4_rental_avg[data['_id']] = data['avg']
 
 for data in list(return_avg_by_rental_office):
-    # return_office_id.append(data['_id'])
-    # avgs.append(data['avg'])
     if rental_return_avg_sub.get(data['_id']):
         sub = rental_return_avg_sub[data['_id']] - data['avg']
         rental_return_avg_sub[data['_id']] = sub
+        pipe4_return_avg[data['_id']] = data['avg']
     else:
         rental_return_avg_sub[data['_id']] = -data['avg']
+        pipe4_return_avg[data['_id']] = data['avg']
+rental_return_avg_top = sorted(rental_return_avg_sub.items(), key=(lambda x: x[1]), reverse=True)
+count = 0
+for key, value in rental_return_avg_top:
+    if type(key) != str and key is not None:
+        if key <= 5000:
+            office_id_avg.append(value)
+            office_id.append(key)
+            count += 1
+            group_office_id_high[key] = 1
 
-for key, value in rental_return_avg_sub.items():
-    # print("key", key, value)
-    if type(key) != str:
-        office_id_avg.append(value)
-        office_id.append(key)
+    if count >= 10:
+        break
+
+rental_return_avg_top = sorted(rental_return_avg_sub.items(), key=(lambda x: x[1]), reverse=False)
+count = 0
+for key, value in rental_return_avg_top:
+    if type(key) != str and key is not None:
+        if key <= 5000:
+            office_id_avg.append(value)
+            office_id.append(key)
+            count += 1
+            group_office_id_low[key] = 1
+
+    if count >= 10:
+        break
+
+for key in office_id:
+    pipe4_rental_avg_list.append(pipe4_rental_avg.get(key, 0))
+    pipe4_return_avg_list.append(pipe4_return_avg.get(key, 0))
 
 df = pd.DataFrame(rental_office_location)
 for i in df.index[:]:
@@ -147,131 +176,60 @@ for year in ["2015", "2016", "2017", "2018", "2019", "2020", "2021"]:
     pipe6_office_avg.append(use_avg_by_install_date[year])
 # -------------------------------------------------------------------------------------------
 fig = make_subplots(rows=2, cols=2,
-                    subplot_titles=('날짜별 대여소별 월 평균 사용량', '날짜별 신규 가입자 수', '반납량 대여량 차이', '설치 날짜별 평균 사용량'),
+                    subplot_titles=('날짜별 대여소별 월 평균 사용량', '날짜별 신규 가입자 수', '대여량 반납량 순위', '설치 날짜별 평균 사용량'),
                     specs=[[{"type": "bar"}, {"type": "bar"}], [{"type": "scatter"}, {"type": "pie"}]])
+
 # 1,2 번 시각화
 # 날짜별 대여소별 월 평균 사용량 날짜별 신규 가입자 수
 fig.add_trace(go.Bar(
     x=dates,
-    y=totals, showlegend=False),
+    y=totals, legendgroup='1', showlegend=False),
     row=1, col=1)
 fig.add_trace(go.Bar(
     x=date,
-    y=registers_total, showlegend=False),
+    y=registers_total, legendgroup='2', showlegend=False),
     row=1, col=2)
 
 # 4 번 시각화
 # 반납량 대여량 차이
 fig.add_trace(go.Scatter(
-    x=office_id, y=office_id_avg, showlegend=False),
+    x=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], y=office_id_avg[:10], text=office_id[:10], textposition='middle center',
+    showlegend=False),
     row=2, col=1)
-# fig2 = go.Figure(data=[go.Scatter(
-#     x=office_id, y=office_id_avg,
-#     mode='markers',
-#     marker_size=office_id_avg
-# )])
-# fig2.show()
-
+fig.add_trace(go.Scatter(
+    x=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], y=office_id_avg[10:], text=office_id[10:], textposition='middle center',
+    showlegend=False),
+    row=2, col=1)
 # 6 번 시각화
 # 설치 날짜별 평균 사용량
 fig.add_trace(go.Pie(
-    values=pipe6_office_avg, labels=["2015", "2016", "2017", "2018", "2019", "2020", "2021"]),
+    values=pipe6_office_avg, labels=["2015", "2016", "2017", "2018", "2019", "2020", "2021"], legendgroup='4'),
     row=2, col=2)
-#     x=pipe6_office_id, y=pipe6_office_install_date,
-#     mode='markers',
-#     marker_size=pipe6_office_avg_month
-# )])
-# fig3.show()
 
 fig['layout']['xaxis']['title'] = '년도'
 fig['layout']['yaxis']['title'] = '사용량'
 fig['layout']['xaxis2']['title'] = '년도'
 fig['layout']['yaxis2']['title'] = '신규 가입자'
-fig['layout']['xaxis3']['title'] = '대여소 번호'
+fig['layout']['xaxis3']['title'] = '대여량 반납량 순위'
 fig['layout']['yaxis3']['title'] = '대여량 반납량 차이'
-# fig['layout']['xaxis4']['title'] = 'label x ax4'
-# fig['layout']['yaxis4']['title'] = 'label y ax4'
-fig.update_layout(legend=dict(
-    bordercolor='black',
-    borderwidth=1,
-    orientation="v",
-    yanchor="bottom",
-    x=0.95,
-    y=0.2,
-    xanchor="center",
-))
-# fig.show()
+fig.update_layout(
+    legend_tracegroupgap=180,
+    legend=dict(
+        bordercolor='black',
+        borderwidth=1,
+        orientation="v",
+        yanchor="bottom",
+        x=0.95,
+        y=0.2,
+        xanchor="center",
+    ),
+)
 fig.write_html("graph.html")
-# fig = go.Figure(data=[go.Scatter(
-#     x=dates, y=pipe1_office_id,
-#     mode='markers',
-#     marker_size=totals
-# )])
-# fig.show()
-
-# data1 = [go.Bar(
-#     x=dates,
-#     y=totals
-# )]
-#
-# fig = go.Figure(data=data1)
-# fig.show()
-#
-# data2 = [go.Bar(
-#     y=registers_total,
-#     x=date
-# )]
-#
-# fig2 = go.Figure(data=data2)
-#
-# fig2.show()
-#
-# data3 = [go.Bar(
-#     x=office_name,
-#     y=avgs
-# )]
-#
-# fig3 = go.Figure(data=data3)
-#
-# fig3.show()
-#
-# data4 = [go.Bar(
-#     x=office_name,
-#     y=avgs
-# )]
-#
-# fig4 = go.Figure(data=data4)
-
-# fig4.show()
-
-# pipe = [{'$group': {'_id': "$가입일자", 'total': {'$sum': "$가입 수"}}},
-#         {'$project': {'_id': 0, 'registers': "$_id", 'total': 1}}, {'$sort': {'registers': 1}}]
-# result = database.new_sub.aggregate(pipe)
-#
-# totals = []
-# registers = []
-# df = DataFrame(list(result))
-
-
-# for data in list(result):
-#     totals.append(data['total'])
-#     registers.append(data['registers'])
-# print(totals)
-# print(registers)
-# data = [go.Bar(
-#     x=registers,
-#     y=totals
-# )]
-# fig = go.Figure(data=data)
-# fig.show()
 
 ##지도에 대여소 표시
-## df = pd.read_csv('data/rental_office.csv', encoding='utf-8')
-## df.head()
-## geo_data = 'data/seoul_geo.json'
 # 서울중심
 center = [37.541, 126.986]
-m = folium.Map(location=center, min_zoom=10, height=1000)
+m = folium.Map(location=center, min_zoom=10, width='100%', height='100%')
 # 마커 클러스터 사용
 marker_cluster = MarkerCluster(control=False).add_to(m)
 # 그룹만들기
@@ -281,66 +239,44 @@ g2 = plugins.FeatureGroupSubGroup(marker_cluster, '평균 대여량 이상')
 m.add_child(g2)
 
 m.fit_bounds([[37.443036, 126.856744], [37.646073, 127.100117]])
-# office_avg_month[df.loc[i, 'rental_office_id']]
 for i in df.index[:]:
     # 위도 경도 값 없을시 건너뛰기
     if (df.loc[i, 'latitude']) and (df.loc[i, 'longitude']):
         install_date = rental_office_install_date[df.loc[i, 'rental_office_id']]
         use_avg = use_avg_by_install_date[install_date[:4]]
         avg_month = office_avg_month.get(df.loc[i, 'rental_office_id'], 0)
-
         rental_office_explain = f"대여소 명 : {df.loc[i, 'rental_office_name']}" \
-                                f"<br>월 평균 대여량 : {avg_month}" \
+                                f"<br>월 평균 대여량 : {int(avg_month)}" \
                                 f"<br>설치 시기 : {install_date}" \
-                                f"<br>같은 설치시기인 대여소 평균 대여량: {use_avg}" \
-                                f"<br>대여량 반납량 차이: {rental_return_avg_sub.get(df.loc[i, 'rental_office_id'], 0)}"
+                                f"<br>같은 설치시기인 대여소 평균 대여량: {int(use_avg)}" \
+                                f"<br>평균 대여량 반납량 차이: {int(rental_return_avg_sub.get(df.loc[i, 'rental_office_id'], 0))}"
         iframe = folium.IFrame(
-            rental_office_explain)
-        popup = folium.Popup(iframe, min_width=500, max_width=500, max_height=300, script=True)
+            rental_office_explain, height='100%')
+        popup = folium.Popup(iframe, min_width=500, max_width=500, max_height=160, min_height=160, script=True)
+        color = ''
+        if group_office_id_high.get(df.loc[i, 'rental_office_id'], 0) != 0:
+            color = 'lightred'
+        elif group_office_id_low.get(df.loc[i, 'rental_office_id'], 0) != 0:
+            color = 'lightgray'
+
         if avg_month < use_avg:
-            folium.Marker(
+            if color == '':
+                color = 'lightblue'
+            marker = folium.Marker(
                 location=[df.loc[i, 'latitude'], df.loc[i, 'longitude']],
                 popup=popup,
-                icon=folium.Icon(color='cadetblue', icon='ok')
+                icon=folium.Icon(color=color, icon='ok')
             ).add_to(g1)
         else:
-            folium.Marker(
+            if color == '':
+                color = 'lightgreen'
+            marker = folium.Marker(
                 location=[df.loc[i, 'latitude'], df.loc[i, 'longitude']],
                 popup=popup,
-                icon=folium.Icon(color='lightgreen', icon='ok')
+                icon=folium.Icon(color=color, icon='ok')
             ).add_to(g2)
 
 folium.LayerControl(collapsed=False).add_to(m)
 # html 파일로 저장
 m.save("a.html")
-## 마커 표시
-# for i in df.index[:]:
-#     if not pd.isna(df.loc[i, '위도']) and not pd.isna(df.loc[i, '경도']):
-#         folium.Marker(
-#             location=[df.loc[i, '위도'], df.loc[i, '경도']],
-#             popup=df.loc[i, '대여소 번호'],
-#             icon=folium.Icon(color='cadetblue', icon='ok')
-#         ).add_to(marker_cluster)
-
-##원 표시
-# for i in df.index[:]:
-#     # print(df.loc[i,'위도'])
-#     # print(df.loc[i,'경도'])
-#     # print(df.loc[i, '대여소 번호'])
-#     # print(i)
-#     if not pd.isna(df.loc[i, '위도']) and not pd.isna(df.loc[i, '경도']):
-#         folium.Circle(
-#             location=[df.loc[i, '위도'], df.loc[i, '경도']],
-#             tooltip=df.loc[i, '대여소 번호'],
-#             radius=200
-#         ).add_to(m)
-
-## 등치맵 사용
-# folium.Choropleth(
-#     geo_data=geo_data,
-#     data=df,
-#     columns=('자치구', '대여소 번호'),
-#     key_on='feature.properties.name',
-#     fill_color='BuPu',
-#     legend_name='자치구',
-# ).add_to(m)
+webbrowser.open_new_tab("exe.html")
